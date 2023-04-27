883b3a8ea16bf29df27dbce0a91196afff0575ff
NingNEFU <zhaoning@NEFU.edu.cn>
2023-04-27 16:26:07 +0800

    project

---
+++ include/other1.h
@@ -0,0 +1,6 @@
+#ifndef OTHER1_H
+#define OTHER1_H
+#include <stdio.h>
+void welcome();
+#endif
+
---
+++ src/other1.c
@@ -0,0 +1,5 @@
+#include "other1.h"
+void welcome() {
+        printf( "Welcome to the world of Linux\n" );
+}
+
